284c51016c87064dbbe9fbee1631ae11f1c6e8cc
Simranmohd <120707251+Simranmohd@users.noreply.github.com>
2024-08-05 22:25:06 +0530

    Update StringCalculator.Tests.c

--- StringCalculator.Tests.c
+++ StringCalculator.Tests.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <assert.h>
+#include "StringCalculator.h"
+
+void testEmptyString() {
+    assert(Add("") == 0);
+}
+
+void testOneNumber() {
+    assert(Add("1") == 1);
+}
+
+void testTwoNumbers() {
+    assert(Add("1,2") == 3);
+}
+
+void testMultipleNumbers() {
+    assert(Add("1,2,3,4,5") == 15);
+}
+
+void testNewlineAsDelimiter() {
+    assert(Add("1\n2,3") == 6);
+}
+
+void testCustomDelimiter() {
+    assert(Add("//;\n1;2") == 3);
+}
+
+void runTests() {
+    testEmptyString();
+    testOneNumber();
+    testTwoNumbers();
+    testMultipleNumbers();
+    testNewlineAsDelimiter();
+    testCustomDelimiter();
+    printf("All tests passed!\n");
+}
+
+int main() {
+    runTests();
+    return 0;
+}
+
+
 
